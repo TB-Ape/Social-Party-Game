2d43a62be9ad58ac526979fa68f4db06de025398
TB-Ape <57174832+TB-Ape@users.noreply.github.com>
2025-10-28 09:49:19 +0100

    Add whois game definition

---
+++ games/whois.ts
@@ -0,0 +1,9 @@
+export default {
+  definition: {
+    id: "whois",
+    title: "Who is?",
+    description: "A social voting game.",
+    phases: ["prompt", "vote", "results"],
+    prompts: [{ id: "1", text: "Who is the funniest?" }]
+  }
+}
